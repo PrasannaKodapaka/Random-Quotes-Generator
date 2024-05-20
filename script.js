7340aedc93a217b0ca043aee8d6d9a845a75dc64
Prasanna Kodapaka <sriprasanna1616@gmail.com>
2024-05-20 13:09:10 +0530

    RandomQuotes

---
+++ script.js
@@ -0,0 +1,18 @@
+async function getQuote(){
+    const quoteDiv=document.getElementById('quote')
+
+    try{
+        const response=await fetch('https://api.quotable.io/random')
+        const data=await response.json()
+        console.log(data)
+        // const quoteText=data.content;
+        // const quoteAuthor=data.author;
+        quoteDiv.innerHTML=`
+        <p class='quote-text'>${data.content}</p>
+        <p class='quote-author'>${data.author}</p>
+        `
+    }catch(error){
+        console.log('Error fetching quote',error)
+        quoteDiv.innerHTML='<p>Failed to fetch a quote.Please try again later</p>';
+    }
+}
